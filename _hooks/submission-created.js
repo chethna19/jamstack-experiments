ebfcf3af6ca805b21d3dbaf3f1460482815f8a83
Filip Zrůst <frzng@me.com>
2018-05-30 23:26:15 +0200

    Replace AWS S3 code with console.log

--- _hooks/submission-created.js
+++ _hooks/submission-created.js
@@ -1,11 +1,4 @@
 exports.handler = function(event, context, callback) {
-  const AWS = require('aws-sdk')
-  const s3 = new AWS.S3()
-
-  s3.putObject({
-    Bucket: process.env['TEST_BUCKET'],
-    Key: 'event.json',
-    Body: JSON.stringify(event, null, 2),
-    ContentType: 'application/json'
-  }, callback)
+  console.log('Event: %j', event)
+  callback()
 }
