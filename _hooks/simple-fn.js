49565ef2334ab7e77bc74e54142470fc398fe42c
Filip Zrůst <frzng@me.com>
2018-06-02 09:48:38 +0200

    Try request body parsing

--- _hooks/simple-fn.js
+++ _hooks/simple-fn.js
@@ -1,7 +1,14 @@
+const querystring = require('querystring')
+
 exports.handler = function simpleFn(event, context, callback) {
   console.log("Event: %j", event);
   console.log("Context: %j", context);
+
   callback(null, {
-    "statusCode": 200
+    "statusCode": 200,
+    "headers": {
+      "Content-Type": "application/json"
+    },
+    "body": JSON.stringify(querystring.parse(event["body"]), null, 2)
   })
 }
