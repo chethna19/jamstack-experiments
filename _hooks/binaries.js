456a8911332fca3ada6b9323d851e3d8042f6758
Filip Zrůst <frzng@me.com>
2018-06-23 14:39:43 +0200

    Read files in directories from PATH

--- _hooks/binaries.js
+++ _hooks/binaries.js
@@ -1,7 +1,31 @@
-function getBinaries(event, context, callback) {
-  console.log("PATH =", process.env["PATH"])
+const fs = require("fs")
 
-  callback()
+function getFiles(dir) {
+  return new Promise((res, rej) => {
+    fs.readdir(dir, (err, files) => {
+      if (err) return rej(err)
+
+      res(files)
+    })
+  })
+}
+
+function getBinaries(event, context, callback) {
+  Promise.all(
+    process.env["PATH"]).
+      split(":").
+      map(getFiles)
+  ).then((allFiles) => {
+    callback(null, {
+      "statusCode": 200,
+      "headers": {
+        "Content-Type": "text/plain; charset=utf8")
+      },
+      "body": allFiles.
+        reduce((flat, files) => flat.concat(files), []).
+        join("\n")
+    })
+  }).catch(callback)
 }
 
 exports.handler = getBinaries
