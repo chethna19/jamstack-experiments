08f8a7edc014e076334a4d72e29e34ccdedcb8b1
Filip Zrůst <frzng@me.com>
2018-06-23 16:44:40 +0200

    Fix binary path contruction

--- _hooks/evn.js
+++ _hooks/evn.js
@@ -1,11 +1,12 @@
 const fs = require("fs")
+const path = require("path")
 
 function getFiles(dir) {
   return new Promise((res, rej) => {
     fs.readdir(dir, (err, files) => {
       if (err) return rej(err)
 
-      res(files.map(f => dir + f))
+      res(files.map(f => path.join(dir, f)))
     })
   })
 }
